0673481658ce6bad1ff1d8fe45e2ef58735a84c5
Cezar Garrido <cezar.cgb18@outlook.com>
2019-08-08 09:27:45 -0300

    :pencil: Criação do layout do aplicativo

---
+++ api/entities/aluno.go
@@ -0,0 +1,12 @@
+package entities
+
+
+
+type Aluno struct {
+	ID int64
+	Nome string
+	Email string
+	Rgm string
+	Senha string
+	
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:app/circular_image.dart';
+import 'package:app/menu_page.dart';
+import 'package:flutter/material.dart';
+import 'package:app/zoom_scaffold.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Zoom Menu',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  MenuController menuController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    menuController = new MenuController(
+      vsync: this,
+    )..addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    menuController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => menuController,
+      child: ZoomScaffold(
+        menuScreen: MenuScreen(),
+        contentScreen: Layout(
+            contentBuilder: (cc) => Container(
+                  color: Colors.grey[200],
+                  child: Container(
+                    color: Colors.grey[200],
+                  ),
+                )),
+      ),
+    );
+  }
+}
