4ea504bd21af0f63402fe6e452840be7ec7ec14f
Cezar Garrido <cezar.cgb18@outlook.com>
2019-08-14 13:13:51 -0300

    Buscando dados de contato do aluno

--- crawler/vasculhador.go
+++ crawler/vasculhador.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -20,7 +21,64 @@ type Client struct {
 type Aluno struct {
 	Nome string
 }
+type Contato struct {
+	Tipo  string
+	Valor string
+}
 
+/*
+# th: Nome
+# td: CEZAR GARRIDO BRITEZ
+# th: Data de Nascimento
+# td: 28/12/1997
+# th: Sexo
+# td: Masculino
+# th: Nome do Pai
+# td: VITOR BRITEZ
+# th: Nome da Mãe
+# td: MARIANA GARRIDO
+# th: Estado Civil
+# td: Solteiro(a)
+# th: Nacionalidade
+# td: BRASILEIRO
+# th: Naturalidade
+# td: PARANHOS/MS
+# th: Fenótipo *
+# td: AmarelaBrancaIndígenaNão declaradoPardaPreta
+# th: CPF
+# td: 050.433.691-67
+# th: DOCUMENTO DE IDENTIFICAÇÃO
+# th: RG
+# td: 2.225.228
+# th: Órgão Emissor
+# td: SEJUSP
+# th: Estado
+# td: MS
+# th: Data de Emissão
+# td:
+# th: TÍTULO ELEITORAL
+# th: Número
+# td: 027682941937
+# th: Zona
+# td: 43
+# th: Seção
+# td: 242
+# th: Cidade
+# td: DOURADOS/MS
+# th: Data de Expedição
+# td: 08/03/2018
+# th: ALISTAMENTO MILITAR
+# th: Reservista
+# td: 32000058497-3
+# th: Categoria
+# td:
+# th: Órgão Emissor
+# td: MD
+# th: Série
+# td:
+# th: Data de Expedição
+# td: 30/01/2018
+*/
 func NewClient(rgm, senha string) (Client, error) {
 	client := Client{}
 	param := url.Values{}
@@ -75,40 +133,77 @@ func (c Client) FindAluno() (*Aluno, error) {
 	return parserAluno(string(body))
 }
 
+//parserAluno: Função que pega os dados do aluno
 func parserAluno(html string) (*Aluno, error) {
 	aluno := &Aluno{}
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
 	if err != nil {
 		return aluno, err
 	}
-	doc.Find("table#table_event_form").Each(func(index int, tablehtml *goquery.Selection) {
-		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-			band, ok := rowhtml.Attr("id")
-			fmt.Println("# ok:", ok, "band:", band)
-			if !ok {
-				rowhtml.Find("th").Each(func(indexth int, thhtml *goquery.Selection) {
-					fmt.Println(strings.Join(strings.Fields(thhtml.Text()), " "))
-				})
-				rowhtml.Find("td").Each(func(indextd int, tdhtml *goquery.Selection) {
-					fmt.Println(strings.Join(strings.Fields(tdhtml.Text()), " "))
-				})
-			}
-
-		})
+	doc.Find("body.uc table.event_form tbody tr").Each(func(index int, tablehtml *goquery.Selection) {
+		if index <= 26 {
+			tablehtml.Find("th").Each(func(indexth int, thhtml *goquery.Selection) {
+				//fmt.Println("# th:", strings.Join(strings.Fields(thhtml.Text()), " "))
+			})
+			tablehtml.Find("td").Each(func(indextd int, tdhtml *goquery.Selection) {
+				//fmt.Println("# td:", strings.Join(strings.Fields(tdhtml.Text()), " "))
+			})
+		}
 	})
+	parserContatosAluno(html)
 	return aluno, nil
 }
 
+//parserContatosAluno: função que pega os dados de contato do aluno
+func parserContatosAluno(html string) (*Contato, error) {
+	contato := &Contato{}
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		return contato, err
+	}
+	doc.Find("tbody#SubDatasetField1_tbody").Each(func(index int, tablehtml *goquery.Selection) {
+		fmt.Println("# SubDatasetField1_tbody:", strings.Join(strings.Fields(tablehtml.Text()), " "))
+		tablehtml.Find("tr").Each(func(indexth int, trhtml *goquery.Selection) {
+			trhtml.Find("td").Each(func(indexth int, tdhtml *goquery.Selection) {
+				tdhtml.Find("input").Each(func(indexth int, inputhtml *goquery.Selection) {
+					band, ok := inputhtml.Attr("value")
+					if ok {
+						if band != "f" && band != "t" {
+							fmt.Println("# td input:", strings.Join(strings.Fields(band), " "))
+							fmt.Println("is Phone:", isPhoneNumber(strings.Join(strings.Fields(band), " ")))
+						}
+					}
+				})
+			})
+		})
+	})
+	return contato, nil
+}
+
+//checkLoginError: Função que recebe o html retornado na pagina de login
+//e checa se existe a class error no html,
+//se a class existir retorna o texto que esta na class e false,
+//se não, retorna a mensagem de Bem-vindo e true
 func checkLoginError(html string) (string, bool) {
 	var isLoggged = true
 	var msg = "Bem-vindo"
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
 	if err != nil {
-		return "", false
+		return "Erro ao parsear html de login", false
 	}
 	doc.Find(".error").Each(func(index int, errorhtml *goquery.Selection) {
 		msg = strings.Join(strings.Fields(errorhtml.Text()), " ")
 		isLoggged = false
 	})
 	return msg, isLoggged
+}
+
+func isPhoneNumber(number string) (ok bool) {
+	re := regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+	submatch := re.FindStringSubmatch(number)
+	ok = true
+	if len(submatch) < 2 {
+		ok = false
+	}
+	return
 }
